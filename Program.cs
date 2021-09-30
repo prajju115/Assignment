5831b59f7dcf9417909aef3f8ad19b2f3ea4e898
Prajwal M <68725816+prajju115@users.noreply.github.com>
2021-09-30 07:44:23 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Circle_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius, area;
+            Console.WriteLine("Enter the radius");
+            radius = Convert.ToDouble(Console.ReadLine());
+
+           area = Math.PI * radius *radius; 
+
+            Console.WriteLine("Area{0} is {1}",radius,area);
+            
+        }
+    }
+}
