b5f4c6fec7281696808f7ff03ac52a66ab24431c
Prajwal M <68725816+prajju115@users.noreply.github.com>
2021-09-30 07:47:16 +0530

    Add files via upload

---
+++ holiday.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holiday
+{
+    class holiday
+    {
+        static void Main1()
+        {
+            Console.WriteLine("enter the day");
+            string day = Console.ReadLine();
+            int n = int.Parse(day);
+            if (n == 0 || n > 7)
+            {
+                Console.WriteLine("invalid day");
+            }
+            else if (n <= 5)
+            {
+                Console.WriteLine("working day");
+            }
+            else
+                Console.WriteLine("holiday");
+        }
+    }
+}
