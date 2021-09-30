666f7ca7313a0be751674afd34a1bf0fbe853950
Prajwal M <68725816+prajju115@users.noreply.github.com>
2021-09-30 07:45:42 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,12 @@
-﻿using System;
+﻿//using System;
 
-namespace Circle_Program
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            double radius, area;
-            Console.WriteLine("Enter the radius");
-            radius = Convert.ToDouble(Console.ReadLine());
-
-           area = Math.PI * radius *radius; 
-
-            Console.WriteLine("Area{0} is {1}",radius,area);
-            
-        }
-    }
-}
+//namespace Holiday
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello World!");
+//        }
+//    }
+//}
