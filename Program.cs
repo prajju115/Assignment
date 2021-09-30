1cd794d3ee8b84734fa6af63a9803fa156804d5a
Prajwal M <68725816+prajju115@users.noreply.github.com>
2021-09-30 07:50:59 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,22 @@
 ﻿using System;
-
-namespace Circle_Program
+public class Ecercise5
 {
-    class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int n, i, m = 0, flag = 0;
+        Console.Write("Enter the Number ");
+        n = int.Parse(Console.ReadLine());
+        m = n / 2;
+        for (i = 2; i <= m; i++)
         {
-            double radius, area;
-            Console.WriteLine("Enter the radius");
-            radius = Convert.ToDouble(Console.ReadLine());
-
-           area = Math.PI * radius *radius; 
-
-            Console.WriteLine("Area{0} is {1}",radius,area);
-            
+            if (n % i == 0)
+            {
+                Console.Write("False");
+                flag = 1;
+                break;
+            }
         }
+        if (flag == 0)
+            Console.Write("True");
     }
 }
