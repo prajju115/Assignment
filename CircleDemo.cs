031cd3455001c5b7954741537aee6a88127f7bca
Prajwal M <68725816+prajju115@users.noreply.github.com>
2021-10-01 07:35:59 +0530

    Add files via upload

---
+++ CircleDemo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Assignment
+{
+    class CircleDemo
+    {
+        double radius;
+        const double PI = 3.142;
+        public void SetRadius(double radius)
+        {
+            this.radius = radius;
+        }
+        public double GetRadius()
+        {
+            return radius;
+        }
+        public double CalcDiameter()
+        {
+            return 2 * radius;
+        }
+        public double CalcArea()
+        {
+            return PI * radius * radius;
+        }
+    }
+    class Circle
+    {
+        static void Main(string[] args)
+        {
+            CircleDemo c1 = new CircleDemo();
+            c1.SetRadius(2);
+            Console.WriteLine(c1.GetRadius());
+            Console.WriteLine(c1.CalcDiameter());
+            Console.WriteLine(c1.CalcArea());
+
+        }
+    }
+}
